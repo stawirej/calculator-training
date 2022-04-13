443dca1e30864e63e6970ea0844f2be37b50962c
Piotr Stawirej <piotr.stawirej@gmail.com>
2022-04-13 15:09:12 +0200

    chore: update dependencies

--- src/test/java/calculator/Calculator_Scenarios.java
+++ src/test/java/calculator/Calculator_Scenarios.java
@@ -4,10 +4,11 @@ import static org.assertj.core.api.BDDAssertions.then;
 
 import org.junit.jupiter.api.Test;
 
-final class CalculatorScenarios {
+final class Calculator_Scenarios {
 
     @Test
-    void readyForTraining(){
+    void Ready_for_training() {
+
         var description = "I am ready for training. Environment is configured successfully :)";
         then(description).isNotBlank();
     }
