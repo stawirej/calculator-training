f9a12588696998044d66b37736016f6a82a8373a
Piotr Stawirej <piotr.stawirej@gmail.com>
2022-04-13 15:11:58 +0200

    chore: fix test displaying

--- src/test/java/calculator/Calculator_Scenarios.java
+++ src/test/java/calculator/Calculator_Scenarios.java
@@ -2,8 +2,11 @@ package calculator;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 final class Calculator_Scenarios {
 
     @Test
